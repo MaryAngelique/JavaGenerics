cd082124915e3655a2fe9b6933cc62d34cb7e474
Angelique Abacajan <ma_utsey@yahoo.com>
2022-10-28 22:27:45 -0400

    Migrated Baseball, Football, Sovver, and Team classes from GenericClass to Challege package

---
+++ src/timbuchalka/GenericClass/Challenge/BaseballPlayer.java
@@ -0,0 +1,11 @@
+package timbuchalka.GenericClass.Challenge;
+
+import timbuchalka.GenericClass.Player;
+
+public class BaseballPlayer extends Player {
+
+    public BaseballPlayer(String name) {
+        super(name);
+    }
+
+}
